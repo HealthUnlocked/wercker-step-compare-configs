8001873b50e022815a0b35cfbd7e85f02ec17699
Wojtek Kruk <krukon@gmail.com>
2016-07-28 13:00:25 +0200

    Modified step to compare config file residing on S3

--- run.sh
+++ run.sh
@@ -1,21 +1,29 @@
 if [ ! -n "$WERCKER_COMPARE_CONFIGS_SAMPLE" ]; then
-  error 'Please specify sample config file (absolute or relative path)'
+  echo 'Please specify sample config file (absolute or relative path)'
   exit 1
 fi
 
 if [ ! -n "$WERCKER_COMPARE_CONFIGS_ACTUAL" ]; then
-  error 'Please specify sample config file (absolute path)'
+  echo 'Please specify the location of config file on S3'
   exit 1
 fi
 
-ACTUAL_CONFIG=`ssh -i $PRIVATEKEY_PATH -o StrictHostKeyChecking=no -o UserKnownHostsFile=no hu@$HOSTNAME "cat $WERCKER_COMPARE_CONFIGS_ACTUAL"`
-ACTUAL_PROPERTIES=`echo "$ACTUAL_CONFIG" | grep -ioE "^[^#][a-z0-9 _-]+=" | sort`
-SAMPLE_PROPERTIES=`grep -ioE "^[^#][a-z0-9 _-]+=" $WERCKER_COMPARE_CONFIGS_SAMPLE | sort`
-if [ `comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -3 | wc -l` -gt "0" ]; then
-	echo -e '\n/etc/ecliptic.properties file needs to be updated!'
-	echo -e '\n\nProperties that need to be added:\n'
-	comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -13
-	echo -e '\n\nProperties that need to be removed:\n'
-	comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -23
-	exit 1;
+if ! which aws > /dev/null ; then
+  echo 'AWS CLI not found!'
+  exit 1
 fi
+
+ACTUAL_CONFIG=$(mktemp)
+aws s3 cp $WERCKER_COMPARE_CONFIGS_ACTUAL $ACTUAL_CONFIG > /dev/null
+ACTUAL_PROPERTIES=`cat $ACTUAL_CONFIG | grep -ioE "^[^#][a-z0-9 _-]+=" | sort`
+SAMPLE_PROPERTIES=`grep -ioE "^[^#][a-z0-9 _-]+=" $WERCKER_COMPARE_CONFIGS_SAMPLE | sort`
+rm $ACTUAL_CONFIG
+if [ `comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -3 | wc -l` -gt "0" ]; then
+  echo -e "\n$WERCKER_COMPARE_CONFIGS_ACTUAL file needs to be updated!"
+  echo -e '\nProperties that need to be added:\n'
+  comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -13
+  echo -e '\nProperties that need to be removed:\n'
+  comm <(echo "$ACTUAL_PROPERTIES") <(echo "$SAMPLE_PROPERTIES") -23
+  exit 1;
+fi
+echo "Config file is up to date!"
